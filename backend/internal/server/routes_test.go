50073ecfe23340a88815999f18aafff956764b57
Patrick Luz <eu@patrickluz.dev>
2024-11-23 15:46:05 -0300

    Initial files

---
+++ backend/internal/server/routes_test.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestHandler(t *testing.T) {
+	app := fiber.New()
+
+	s := &FiberServer{App: app}
+
+	app.Get("/", s.HelloWorldHandler)
+	req, err := http.NewRequest("GET", "/", nil)
+
+	if err != nil {
+		t.Fatalf("error creating request. Err: %v", err)
+	}
+
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatalf("error making request to server. Err: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", resp.Status)
+	}
+	expected := "{\"message\":\"Hello World\"}"
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("error reading response body. Err: %v", err)
+	}
+	if expected != string(body) {
+		t.Errorf("expected response body to be %v; got %v", expected, string(body))
+	}
+}
